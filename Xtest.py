9f1b08456ccd372a8845ace0ba76f5f4331e438d
Pierre Rochet <pirocheto@gmail.com>
2023-11-18 17:27:19 +0100

    test model card

--- Xtest.py
+++
@@ -1,7 +0,0 @@
-import joblib
-from huggingface_hub import hf_hub_download
-
-REPO_ID = "pirocheto/phishing-url-detection"
-FILENAME = "model.onnx"
-
-model = hf_hub_download(repo_id=REPO_ID, filename=FILENAME)
