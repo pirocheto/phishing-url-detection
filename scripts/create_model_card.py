d258d5a6600a7988310b33d4e6ac907baaa86541
Pierre Rochet <pirocheto@gmail.com>
2023-11-18 17:27:19 +0100

    test model card

--- scripts/create_model_card.py
+++ scripts/create_model_card.py
@@ -30,63 +30,60 @@ hub_utils.init(
     data=df_test.head(5),
 )
 
-
 hub_utils.add_files("models/model.onnx", dst=dst)
-
-# Create a model card object
-model_card = card.Card(model, metadata=card.metadata_from_config(dst))
-
-# Set metadata for the model card
-model_card.metadata.license = "mit"
-model_card.metadata.inference = False
-model_card.metadata.pipeline_tag = "tabular-classification"
-model_card.metadata.tags = [
-    "tabular-classification",
-    "sklearn",
-    "phishing",
-    "onnx",
-]
 
 
 # Read code snippets from files for model deployment
 with open("scripts/load_model/onnx_model.py") as fp:
     onnx_py_code = fp.read()
 
-# with open("scripts/load_model/onnx_model.js") as fp:
-#     onnx_js_code = fp.read()
-
-# with open("scripts/load_model/joblib_model.py") as fp:
-#     joblib_code = fp.read()
-
-# Add code snippets to the model card
-model_card.add(
-    **{
-        "Model description": "",
-        "Model description/Training Procedure": "",
-        "How to Get Started with the Model": "Below are some code snippets to load the model.",
-        # "How to Get Started with the Model/With joblib (not recommended)": f"```python  \n{joblib_code}  \n```",
-        "How to Get Started with the Model/With ONNX (recommended)/Python": f"```python  \n{onnx_py_code}  \n```",
-        # "How to Get Started with the Model/With ONNX (recommended)/JavaScript": f"```javascript  \n{onnx_js_code}  \n```",
-    },
-)
-
-# Add a plot describing the model architecture
-model_card.add_model_plot(
-    description="This is the architecture of the model loaded by joblib."
-)
-
 # Load metrics from a YAML file
 with open("results/metrics.yaml", "r") as fp:
     metrics = yaml.safe_load(fp)
 
-# Add metrics to the model card
+
+# Create a model card object
+model_card = card.Card(
+    model,
+    metadata=card.metadata_from_config(dst),
+    template=None,
+)
+
+description = """
+The model predicts the probability that a URL is a phishing site using a list of features.
+
+- **Developed by:** [pirocheto](https://github.com/pirocheto)
+- **Model type:** Traditional machine learning
+- **Task:** Tabular classification (Binary)
+- **License:** {{ license }}
+- **Repository:** {{ repo }}
+"""
+
+get_started_onnx = f"```python\n{onnx_py_code}\n```"
+
+sections = {
+    "Model Description": description,
+    "How to Get Started with the Model/With ONNX (recommanded)": get_started_onnx,
+}
+
+
 model_card.add_metrics(**metrics["test"])
+model_card.add(**sections)
 
-# Delete unnecessary sections from the model card
-model_card.delete("Model description/Intended uses & limitations")
-model_card.delete("Model Card Authors")
-model_card.delete("Model Card Contact")
-model_card.delete("Citation")
+metadata = {
+    "license": "mit",
+    "inference": False,
+    "pipeline_tag": "tabular-classification",
+    "tags": [
+        "tabular-classification",
+        "sklearn",
+        "phishing",
+        "onnx",
+    ],
+}
 
-# Save the model card
+for name, value in metadata.items():
+    setattr(model_card.metadata, name, value)
+
+
 model_card.save(Path(dst) / "README.md")
