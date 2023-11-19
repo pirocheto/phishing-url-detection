1cd283b69527c1b694833a49ef27f6cf8dcad5ee
Pierre Rochet <pirocheto@gmail.com>
2023-11-19 04:32:55 +0100

    clean repo

--- scripts/load_model/onnx_model.py
+++ scripts/load_model/onnx_model.py
@@ -1,47 +1,35 @@
+import numpy as np
 import onnxruntime
-import pandas as pd
 from huggingface_hub import hf_hub_download
 
-REPO_ID = "pirocheto/phishing-url-detection"
-FILENAME = "model.onnx"
+# REPO_ID = "pirocheto/phishing-url-detection"
+# FILENAME = "model.onnx"
+# model_path = hf_hub_download(repo_id=REPO_ID, filename=FILENAME)
+
+model_path = "./models/model.onnx"
 
 # Initializing the ONNX Runtime session with the pre-trained model
 sess = onnxruntime.InferenceSession(
-    hf_hub_download(repo_id=REPO_ID, filename=FILENAME),
+    model_path,
     providers=["CPUExecutionProvider"],
 )
 
 # Defining a list of URLs with characteristics
-data = [
-    {
-        "url": "https://www.rga.com/about/workplace",
-        "nb_hyperlinks": 97,
-        "ratio_intHyperlinks": 0.969072165,
-        "ratio_extHyperlinks": 0.030927835,
-        "ratio_extRedirection": 0,
-        "safe_anchor": 25,
-        "domain_registration_length": 3571,
-        "domain_age": 11039,
-        "web_traffic": 178542,
-        "google_index": 0,
-        "page_rank": 5,
-    },
-]
+urls = ["https://www.rga.com/about/workplace"]
 
-# Converting data to a float32 NumPy array
-df = pd.DataFrame(data).set_index("url")
-inputs = df.to_numpy(dtype="float32")
+
+inputs = np.array(urls, dtype="str")
 
 # Using the ONNX model to make predictions on the input data
 probas = sess.run(None, {"X": inputs})[1]
 
 # Displaying the results
-for url, proba in zip(data, probas):
-    print(f"URL: {url['url']}")
+for url, proba in zip(urls, probas):
+    print(f"URL: {url}")
     print(f"Likelihood of being a phishing site: {proba[1] * 100:.2f}%")
     print("----")
 
 # Expected output:
 # URL: https://www.rga.com/about/workplace
-# Likelihood of being a phishing site: 0.89%
+# Likelihood of being a phishing site: 0.25%
 # ----
