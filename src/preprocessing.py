ac02b87a05821338669feb4dc102db45cf6d98b8
Pierre Rochet <pirocheto@gmail.com>
2023-11-19 03:48:41 +0100

    dvc: commit experiment 5432befff6cad27ce2f1df7a89eff3b59da0793c9c50a35b060ae80c1deaf152

--- src/preprocessing.py
+++ src/preprocessing.py
@@ -1,33 +1,27 @@
 import os
+import pickle
 
-import dill
 import dvc.api
 import pandas as pd
 from hydra.utils import instantiate
-from sklearn.preprocessing import FunctionTransformer
 
 # Get the dvc params
 params = dvc.api.params_show(stages="preprocessing")
 path_data_raw = params["path"]["data"]["raw"]
-path_data_transformed = params["path"]["data"]["transformed"]
+feature = params["column_mapping"]["feature"]
 target = params["column_mapping"]["target"]
 
 if __name__ == "__main__":
     # Load the test dataset
-    df_test = pd.read_csv(
-        path_data_raw["test"],
-        index_col=params["column_mapping"]["id"],
-    )
+    df_test = pd.read_csv(path_data_raw["test"])
+
     # Load the training dataset
-    df_train = pd.read_csv(
-        path_data_raw["train"],
-        index_col=params["column_mapping"]["id"],
-    )
+    df_train = pd.read_csv(path_data_raw["train"])
 
     # Seperate data into features and target to be compliant with sklean API
-    X_test = df_test.drop(target, axis=1)
+    X_test = df_test[feature]
     y_test = df_test[target]
-    X_train = df_train.drop(target, axis=1)
+    X_train = df_train[feature]
     y_train = df_train[target]
 
     # 1. Process target
@@ -50,27 +44,13 @@ if __name__ == "__main__":
     X_train = feature_preprocessor.transform(X_train)
     X_test = feature_preprocessor.transform(X_test)
 
+    os.makedirs(params["path"]["results"]["models"]["dir"], exist_ok=True)
+
     path_preprocessor = params["path"]["results"]["models"]["preprocessor"]
+    path_label_encoder = params["path"]["results"]["models"]["label_encoder"]
+
     with open(path_preprocessor, "wb") as fp:
-        dill.dump(feature_preprocessor, fp)
+        pickle.dump(feature_preprocessor, fp)
 
-    # Save the transformed data
-    df_train_transformed = pd.DataFrame(
-        X_train,
-        columns=df_train.columns[:-1],
-        index=df_train.index,
-    )
-    df_test_transformed = pd.DataFrame(
-        X_test,
-        columns=df_test.columns[:-1],
-        index=df_test.index,
-    )
-
-    df_train_transformed[target] = y_train
-    df_test_transformed[target] = y_test
-
-    # Create directory for transformed data
-    os.makedirs(path_data_transformed["dir"], exist_ok=True)
-
-    df_train_transformed.to_csv(path_data_transformed["train"])
-    df_test_transformed.to_csv(path_data_transformed["test"])
+    with open(path_label_encoder, "wb") as fp:
+        pickle.dump(target_preprocessor, fp)
