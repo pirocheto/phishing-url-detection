9240239793cf1742d5ebfa5492523f75427cebc7
Pierre Rochet <pirocheto@gmail.com>
2023-11-19 21:50:13 +0100

    add train model in ci

---
+++ scripts/load_model/load_onnx.py
@@ -0,0 +1,37 @@
+from pathlib import Path
+
+import numpy as np
+import onnxruntime
+from huggingface_hub import hf_hub_download
+
+# REPO_ID = "pirocheto/phishing-url-detection"
+# FILENAME = "model.onnx"
+# model_path = hf_hub_download(repo_id=REPO_ID, filename=FILENAME)
+
+model_path = Path(__file__).parents[2] / "models/model.onnx"
+
+# model_path = "../models/model.onnx"
+
+# Initializing the ONNX Runtime session with the pre-trained model
+sess = onnxruntime.InferenceSession(
+    model_path,
+    providers=["CPUExecutionProvider"],
+)
+
+# Defining a list of URLs with characteristics
+urls = [
+    "https://www.rga.com/about/workplace",
+    "http://www.iracing.com/tracks/gateway-motorsports-park/",
+]
+
+
+inputs = np.array(urls, dtype="str")
+
+# Using the ONNX model to make predictions on the input data
+results = sess.run(None, {"inputs": inputs})[1]
+
+# Displaying the results
+for url, proba in zip(urls, results):
+    print(f"URL: {url}")
+    print(f"Likelihood of being a phishing site: {proba[1] * 100:.2f} %")
+    print("----")
