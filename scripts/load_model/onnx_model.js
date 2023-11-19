4d85ef4a6279a412407e2e6c6c04f6bc8759f5a7
Pierre Rochet <pirocheto@gmail.com>
2023-11-19 03:32:11 +0100

    dvc: commit experiment 4ae349141fdc43d3b46f873529abfb8782641c872d7d6073f7c927654b3e0b7a

--- scripts/load_model/onnx_model.js
+++ scripts/load_model/onnx_model.js
@@ -1,20 +1,6 @@
 const ort = require('onnxruntime-node');
 
-const data = [
-    {
-        "url": "http://rapidpaws.com/wp-content/we_transfer/index2.php?email=/",
-        "nb_hyperlinks": 1,
-        "ratio_intHyperlinks": 1,
-        "ratio_extHyperlinks": 0,
-        "ratio_extRedirection": 0,
-        "safe_anchor": 0,
-        "domain_registration_length": 338,
-        "domain_age": 0,
-        "web_traffic":1853,
-        "google_index": 1,
-        "page_rank": 2,
-    },
-];
+const urls = ["https://www.rga.com/about/workplace", "https://hd1xor4.com/rd/c44979XCTyQ347384YbMC5057DBz8924Sclt660"];
 
 async function main() {
     try {
@@ -23,21 +9,17 @@ async function main() {
         const model_path = "./models/model.onnx";
         const session = await ort.InferenceSession.create(model_path);
         
-        // Creating an ONNX tensor from the input data
-        const inputs = data.map(url => Object.values(url).slice(1));
-        const flattenInputs = inputs.flat();
-        const tensor = new ort.Tensor('float32', flattenInputs, [inputs.length, 10]);
-        
         // Executing the inference session with the input tensor
+        const tensor = new ort.Tensor('string', urls);
         const results = await session.run({"X": tensor});
         const probas = results['probabilities'].data;
-        
+
         // Displaying results for each URL
-        data.forEach((url, index) => {
+        urls.forEach((url, index) => {
             const proba = probas[index * 2 + 1];
             const percent = (proba * 100).toFixed(2);
             
-            console.log(`URL: ${url.url}`);
+            console.log(`URL: ${url}`);
             console.log(`Likelihood of being a phishing site: ${percent}%`);
             console.log("----");
         });
@@ -52,5 +34,5 @@ main();
 
 // Expected output:
 // URL: https://www.rga.com/about/workplace
-// Likelihood of being a phishing site: 0.89%
+// Likelihood of being a phishing site: 0.25%
 // ----
--- src/finalize_model.py
+++ src/finalize_model.py
@@ -3,7 +3,6 @@ import pickle
 import warnings
 
 import dvc.api
-import numpy as np
 import pandas as pd
 from hydra.utils import instantiate
 from skl2onnx import to_onnx
@@ -22,35 +21,22 @@ params = dvc.api.params_show(stages="finalize_model")
 if __name__ == "__main__":
     # Load all Sklearn objects
     classifier = instantiate(params["classifier"])
-    feature_selector = instantiate(params["feature_selection"])
     target_preprocessor = instantiate(params["preprocessing"]["target"])
-
-    feature_preprocessing = params["preprocessing"].get("feature", None)
-    if feature_preprocessing:
-        feature_preprocessor = instantiate(params["preprocessing"]["feature"])
+    feature_preprocessor = instantiate(params["preprocessing"]["feature"])
 
     # 1. Load data
-    df_train = pd.read_csv(
-        params["path"]["data"]["all"],
-        index_col=params["column_mapping"]["id"],
-    )
+    df_train = pd.read_csv(params["path"]["data"]["all"])
 
     # Get features and labels
-    y_train = df_train[params["column_mapping"]["target"]]
-    df_train = df_train.drop(params["column_mapping"]["target"], axis=1)
+    X_train = df_train[params["column_mapping"]["feature"]].values
+    y_train = df_train[params["column_mapping"]["target"]].values
 
     # Encode the labels
     y_train = target_preprocessor.fit_transform(y_train)
 
-    # Keep only the relevant features
-    X_train = feature_selector.fit_transform(df_train, y_train)
-
     # 2. Build and train the model
-    if feature_preprocessing:
-        pipeline = make_pipeline(feature_preprocessor, classifier)
-        model = pipeline.fit(X_train, y_train)
-    else:
-        model = classifier.fit(X_train, y_train)
+    pipeline = make_pipeline(feature_preprocessor, classifier)
+    model = pipeline.fit(X_train, y_train)
 
     # 3. Save the model
     # Save model in pickle format
@@ -59,6 +45,6 @@ if __name__ == "__main__":
         pickle.dump(model, fp)
 
     # Save model in onnx format
-    onx = to_onnx(model, X_train[:1].astype(np.float32), options={"zipmap": False})
+    onx = to_onnx(model, X_train[:1].astype("str"), options={"zipmap": False})
     with open(params["path"]["final_models"]["onnx_model"], "wb") as fp:
         fp.write(onx.SerializeToString())
