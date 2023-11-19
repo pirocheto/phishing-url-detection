d1bf64d81dbc4bd8ca239605db1d6bc18ae4bc09
Pierre Rochet <pirocheto@gmail.com>
2023-11-19 13:27:32 +0100

    clear

--- scripts/load_model/joblib_model.py
+++
@@ -1,7 +0,0 @@
-import joblib
-from huggingface_hub import hf_hub_download
-
-REPO_ID = "pirocheto/phishing-url-detection"
-FILENAME = "model.pkl"
-
-model = joblib.load(hf_hub_download(repo_id=REPO_ID, filename=FILENAME))
