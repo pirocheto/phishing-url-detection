d9a1ae356f46a05b75c878023bff18f546c39b56
prochet <pierre-rochet@outlook.com>
2023-11-29 14:23:52 +0100

    update comments

--- Makefile
+++ Makefile
@@ -1,15 +1,19 @@
+# Download data using DVC
 download_data:
 	dvc repro download_data
 
+# Pull the pickled model and ONNX model using DVC
 pull_model:
 	dvc pull live/model/model.pkl live/model/model.onnx
 
+# Generate a model card using the src/modelcard.py script
 modelcard:
 	python src/modelcard.py
 
+# Generate a report using the src/report.py script
 report:
 	python src/report.py
 
+# Launch the Optuna dashboard using the optuna-dashboard command
 optuna_dashboard:
-	optuna-dashboard notebooks/optunalog/optuna.db
-
+	optuna-dashboard sqlite:///notebooks/optuna.db
--- tests/conftest.py
+++ tests/conftest.py
@@ -3,17 +3,20 @@ import pytest
 from src.helper import load_data, load_onnx_session, load_pickle_model
 
 
+# Fixture to provide a sample of input data (X_sample) for testing
 @pytest.fixture
 def X_sample():
     X, _ = load_data("data/test.parquet")
     return X[:50]
 
 
+# Fixture to provide an ONNX session (onnx_sess) for testing
 @pytest.fixture
 def onnx_sess():
     return load_onnx_session("live/model/model.onnx")
 
 
+# Fixture to provide a pickled model (pkl_model) for testing
 @pytest.fixture
 def pkl_model():
     return load_pickle_model("live/model/model.pkl")
