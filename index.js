ba9877bcd18d4f8b19a969d865e5dac7264ad242
Pierre Rochet <pirocheto@gmail.com>
2023-12-01 19:38:34 +0100

    add results

--- index.js
+++ index.js
@@ -1,16 +1,24 @@
 let ortSession = null;
 
 async function instantiateSess() {
+  document.getElementById("session-loader").style.display = "flex";
   try {
     const model_path =
       "https://huggingface.co/pirocheto/phishing-url-detection/resolve/main/model.onnx";
     ortSession = await ort.InferenceSession.create(model_path);
   } catch (e) {
     document.write(`failed to instantiate ONNX model: ${e}.`);
+  } finally {
+    document.getElementById("session-loader").style.display = "none";
   }
 }
 
 async function predict(url) {
+  document.getElementById("check-button").disabled = true;
+  document.getElementById("check-button").classList.add("opacity-50");
+  document.getElementById("check-loader").style.display = "flex";
+  document.getElementById("check-text").style.display = "none";
+
   try {
     if (!ortSession) {
       await instantiateSess();
@@ -22,6 +30,12 @@ async function predict(url) {
     return probas[1];
   } catch (e) {
     document.write(`failed to perform inference with ONNX model: ${e}.`);
+    return null;
+  } finally {
+    document.getElementById("check-loader").style.display = "none";
+    document.getElementById("check-button").classList.remove("opacity-50");
+    document.getElementById("check-text").style.display = "flex";
+    document.getElementById("check-button").disabled = false;
   }
 }
 
@@ -37,6 +51,9 @@ function isValidURL(input) {
 async function checkPhishing() {
   var urlInput = document.getElementById("urlInput").value;
   var resultElement = document.getElementById("result");
+  var urlElement = document.getElementById("url");
+  var scoreElement = document.getElementById("score");
+  var messageElement = document.getElementById("message");
 
   // Validate the input URL
   if (!isValidURL(urlInput)) {
@@ -47,6 +64,26 @@ async function checkPhishing() {
   // Perform the phishing check
   var probability = await predict(urlInput);
 
-  // Display the probability
-  resultElement.innerText = "Phishing Probability: " + probability.toFixed(2);
+  const warningMessage = `
+    <span class="flex items-center">
+      can be dangerous
+      <img src="assets/warning.svg" class="h-4 w-4 ml-2" /> 
+    </span>`;
+
+  const safeMessage = `
+    <span class="flex items-center">
+      seems to be safe
+      <img src="assets/safe.svg" class="h-4 w-4 ml-2" /> 
+    </span>`;
+
+  const message = probability > 0.5 ? safeMessage : warningMessage;
+
+  urlElement.innerText = urlInput;
+  scoreElement.innerText = probability.toFixed(2);
+  messageElement.innerHTML = message;
+  resultElement.style.display = "block";
+}
+
+if (!ortSession) {
+  instantiateSess();
 }
